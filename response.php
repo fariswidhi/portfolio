64680faa9487ab3a1054c8174f136b08fb6fe4fe
faris <fariswidhiarta123@gmail.com>
2019-08-26 13:25:51 +0700

    x

---
+++ response.php
@@ -0,0 +1,54 @@
+<?php 
+
+$arr = [
+	[
+
+	'title'=>'SafetyEdoker',
+	'image'=>'images/safety.png',
+	'desc'=>'Aplikasi Elektronik Dokumen Kerja PLN'
+],
+	[
+
+	'title'=>'Dirhamjasasetia.com',
+	'image'=>'images/safety.png',
+	'desc'=>'Website Company Profile CV Dirham Jasa Setia'
+	],
+
+	[
+
+	'title'=>'bo.tripleasport.com',
+	'image'=>'images/triplea,png'
+
+	],
+	[
+
+	'title'=>'register.cit.co.id',
+	'image'=>'images/cit-logo.png'
+	],
+	[
+
+	'title'=>'Cerita Rembang',
+	'image'=>'images/ceritarembang.png'
+
+	],
+
+	[
+
+	'title'=>'EazySign',
+	'image'=>'images/signature.png'
+	],
+
+	[
+
+	'title'=>'Phone Arena',
+	'image'=>'images/phone.png'
+	],
+
+	[
+
+	'title'=>'E-Scan Kehadiran Tamu Konfercab GP Ansor XXI Rembang',
+	'image'=>'images/konfercab.png'
+	]
+];
+
+echo json_encode($arr);
